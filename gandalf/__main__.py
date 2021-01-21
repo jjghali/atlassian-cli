739961582a0fa8456fbe24df36e2cf3b27a0dc86
Joshua Ghali <joshua.ghali@desjardins.com>
2021-01-20 21:03:26 -0500

    wip

--- gandalf/__main__.py
+++ gandalf/__main__.py
@@ -1,4 +1,22 @@
-from omission import app
+from sys import argv
+# from gandalf import app
+
+commands = ['help', '-v', 'product']
+
+
+def help():
+    print("this is help")
+
+
+def version():
+    print("app version here")
+
 
 if __name__ == '__main__':
-    app.run()
+    if argv[1].lower() in commands:
+        if argv[1].lower() == "help":
+            help()
+        elif argv[1].lower() == "-v":
+            version()
+    else:
+        help()
