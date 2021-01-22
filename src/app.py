a2b5f1aeefc4e0beb78f284d49c39045ae24244b
Joshua Ghali <jjghali@protonmail.ch>
2021-01-22 15:54:08 -0500

    added yaml config files

--- src/app.py
+++ src/app.py
@@ -2,6 +2,9 @@ import click
 from sys import argv
 from commands import product
 from commands import component
+from commands import releaseNote
+from commands import config
+
 
 @click.group(help="help text here")
 @click.option('--bitbucket-url')
@@ -9,19 +12,24 @@ from commands import component
 @click.option('--atlassian-username')
 @click.option('--atlassian-password')
 @click.pass_context
-def cli(ctx,bitbucket_url, jira_url, atlassian_username,atlassian_password):
-    ctx.ensure_object(dict)    
+def cli(ctx, bitbucket_url, jira_url, atlassian_username, atlassian_password):
+    ctx.ensure_object(dict)
     ctx.obj['BITBUCKET_URL'] = bitbucket_url
     ctx.obj['JIRA_URL'] = jira_url
     ctx.obj['ATLASSIAN_USERNAME'] = atlassian_username
     ctx.obj['ATLASSIAN_PASSWORD'] = atlassian_password
-   
+# TODO: add warning for config file missing if ran locally
+
+
 @cli.command()
 def version():
     print("app version here")
 
+
+cli.add_command(releaseNote.releasenote)
 cli.add_command(product.product)
 cli.add_command(component.component)
+cli.add_command(config.config)
 
 if __name__ == '__main__':
-    cli()    
+    cli()
