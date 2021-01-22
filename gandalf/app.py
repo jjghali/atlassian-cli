436e4cb59ae5aed1fa8d454f751f30b20e3674ff
Joshua Ghali <jjghali@protonmail.ch>
2021-01-22 12:30:29 -0500

    added retrieval of a tag

--- gandalf/app.py
+++ gandalf/app.py
@@ -1,13 +1,18 @@
 import click
 from sys import argv
 from commands import product
+from commands import component
 
 @click.group(help="help text here")
+@click.option('--bitbucket-url')
+@click.option('--jira-url')
 @click.option('--atlassian-username')
 @click.option('--atlassian-password')
 @click.pass_context
-def cli(ctx,atlassian_username,atlassian_password):
+def cli(ctx,bitbucket_url, jira_url, atlassian_username,atlassian_password):
     ctx.ensure_object(dict)    
+    ctx.obj['BITBUCKET_URL'] = bitbucket_url
+    ctx.obj['JIRA_URL'] = jira_url
     ctx.obj['ATLASSIAN_USERNAME'] = atlassian_username
     ctx.obj['ATLASSIAN_PASSWORD'] = atlassian_password
    
@@ -16,6 +21,7 @@ def version():
     print("app version here")
 
 cli.add_command(product.product)
+cli.add_command(component.component)
 
 if __name__ == '__main__':
     cli()    
