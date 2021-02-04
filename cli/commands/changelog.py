419a1811b4f147538f77a2d335c59f713911d05f
Joshua Ghali <jjghali@protonmail.ch>
2021-02-04 17:19:13 -0500

    added jira tasks between tags

--- cli/commands/changelog.py
+++ cli/commands/changelog.py
@@ -1,11 +1,15 @@
 import click
 import json
-from atlassian import Bitbucket
-from atlassian import Jira
-from atlassian import Confluence
+from services import ConfluenceService
+
+skipssl = False
+
 
 @click.group()
 @click.pass_context
 def changelog(ctx):
     """Creates a changelog page on Confluence"""
+    context_parent = click.get_current_context(silent=True)
+    ctx.ensure_object(dict)
+    skipssl = context_parent.obj["skipssl"]
     pass
