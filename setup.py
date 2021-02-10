4252d3333aee44308a44aa6751d471407eb1c2df
Joshua Ghali <jjghali@protonmail.ch>
2021-02-10 14:38:31 -0500

    - added dynamic version setting - Update setup.py - Update publish-pypi.yml - Delete create-release.yml

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import re
+import os
 from setuptools import setup, find_packages
 
 deps = [
@@ -10,10 +12,19 @@ deps = [
     'requests'
 ]
 
+version = ""
+github_ref = os.getenv('GITHUB_REF')
+if github_ref is not None:
+    m = re.search(
+        "(([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?)", github_ref)
+    if m:
+        version = m.group(1)
+else:
+    version = "dev"
 
 setup(
     name='atlcli',
-    version='0.0.2',
+    version=version,
     packages=find_packages(),
     py_modules=['cli', "commands"],
     include_package_data=True,
