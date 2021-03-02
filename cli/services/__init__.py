f634ba7dc362ee3f4b7708087b73e0f18a605a47
Joshua Ghali <joshua.ghali@desjardins.com>
2021-03-02 17:15:05 -0500

    added lead time for changes

--- cli/services/__init__.py
+++ cli/services/__init__.py
@@ -3,5 +3,8 @@ from .confluence_service import ConfluenceService
 from .jira_service import JiraService
 from .sonarqube_service import SonarQubeService
 from .stats_service import StatsService
+<<<<<<< HEAD
 from .powerbi_service import PowerBIService
+=======
+>>>>>>> d2c6d61... added lead time for changes
 from utils import *
