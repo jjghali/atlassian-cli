9bdc3f471bb104c44e6c3dbcc3eeed298113dff9
Joshua Ghali <joshua.ghali@desjardins.com>
2021-03-17 16:34:22 -0400

    Added story points calculation

--- cli/services/powerbi_service.py
+++ cli/services/powerbi_service.py
@@ -11,23 +11,31 @@ class PowerBIService:
         payload = self.prepare_payload(product, version, release_date, lead_time_for_changes, deploy_freq, deploy_freq_date)   
         self.push_to_pbi(payload)
     
-    def push_data_all(self, product, leadtimes_all, deploy_freq_per_release):        
+    def push_data_all(self, product, leadtimes_all, deploy_freq_per_release, storypoints_all):
         
         for key in leadtimes_all:
             l = leadtimes_all[key]
+            
+            story_point = 0
+
+            if key in storypoints_all:
+                story_points = storypoints_all[key]
+
             if "lead-time" in l:
                 payload = self.prepare_payload(product=product, 
                 version=key, 
                 release_date=l["release-date"], 
                 lead_time_for_changes=l["lead-time"],
                 deploy_freq=deploy_freq_per_release[key]["deploy_freq"],
-                deploy_freq_date=deploy_freq_per_release[key]["releaseDate"])
+                deploy_freq_date=deploy_freq_per_release[key]["releaseDate"],
+                story_points=story_points)
             else:
                 payload = self.prepare_payload(product=product, 
                 version=key, 
                 release_date=l["release-date"],                 
                 deploy_freq=deploy_freq_per_release[key]["deploy_freq"],
-                deploy_freq_date=deploy_freq_per_release[key]["releaseDate"])
+                deploy_freq_date=deploy_freq_per_release[key]["releaseDate"],
+                story_points=story_points)
 
             self.push_to_pbi(payload)
 
@@ -38,7 +46,8 @@ class PowerBIService:
             "release-date": release_date,
             "leadtimeforchanges": lead_time_for_changes,
             "deploy-freq":deploy_freq,
-            "deploy-freq-date": deploy_freq_date
+            "deploy-freq-date": deploy_freq_date,
+            "story-points": story_points
         }]
                 
         return json.dumps(payload_json)
